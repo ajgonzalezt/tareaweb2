ea57b142f0630a0a1bec5a5a6d12e1402e52133f
ajgonzalezt <47196362+ajgonzalezt@users.noreply.github.com>
2021-08-23 23:32:55 -0500

    Add files via upload

---
+++ crearClientes.js
@@ -0,0 +1,111 @@
+const fs = require("fs");
+// JSON data
+const data = require("./clientes.json");
+// Build paths
+const { buildPathHtml } = require("./buildPaths");
+
+/**
+ * Take an object which has the following model
+ * @param {Object} item
+ * @model
+ * {
+ *   "idcliente": `Number`,
+ *   "nombrecompania": `String`,
+ *   "nombrecontacto": `String`,
+ * }
+ *
+ * @returns {String}
+ */
+
+const createRow = (item) => `
+  <tr>
+    <td>${item.idcliente}</td>
+    <td>${item.nombrecompania}</td>
+    <td>${item.nombrecontacto}</td>
+  </tr>
+`;
+
+/**
+ * @description Generates an `html` table with all the table rows
+ * @param {String} rows
+ * @returns {String}
+ */
+const createTable = (rows) => `
+  <div class="container">
+        <center>
+            <h2>Clientes</h2>
+        </center>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre de la compañía</th>
+                    <th>Nombre de contacto</th>
+                </tr>
+            </thead>
+            <tbody>
+             ${rows}   
+            </tbody>
+        </table>
+    </div>
+`;
+
+/**
+ * @description Generate an `html` page with a populated table
+ * @param {String} table
+ * @returns {String}
+ */
+const createHtml = (table) => `
+  <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Clientes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+    <body>
+      ${table}
+    </body>
+  </html>
+`;
+
+/**
+ * @description this method takes in a path as a string & returns true/false
+ * as to if the specified file path exists in the system or not.
+ * @param {String} filePath
+ * @returns {Boolean}
+ */
+const doesFileExist = (filePath) => {
+  try {
+    fs.statSync(filePath); // get information of the specified file path.
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export default function executeClientes() {
+  try {
+    /* Check if the file for `html` build exists in system or not */
+    if (doesFileExist(buildPathHtml)) {
+      console.log("Deleting old build file");
+      /* If the file exists delete the file from system */
+      fs.unlinkSync(buildPathHtml);
+    }
+    /* generate rows */
+    const rows = data.map(createRow).join("");
+    /* generate table */
+    const table = createTable(rows);
+    /* generate html */
+    const html = createHtml(table);
+    /* write the generated html to file */
+    fs.writeFileSync(buildPathHtml, html);
+    console.log("Succesfully created an HTML table");
+  } catch (error) {
+    console.log("Error generating table", error);
+  }
+}
---
+++ main.js
@@ -0,0 +1,54 @@
+////import { executeProveedores } from "./createTableProveedores.js";
+
+const fs = require("fs");
+
+const http = require("http");
+
+const axios = require("axios");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    let url = req.url;
+
+    if (url == "/api/proveedores") {
+      axios
+        .get(
+          "https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json"
+        )
+        .then((resp) => {
+          let respuesta = JSON.stringify(resp.data, null, 2);
+          fs.writeFile("proveedores.json", respuesta, "utf8", () => {
+            console.log("Json de proveedores escrito correctamente");
+          });
+
+          $.getScript("createTableClientes.js", () => {
+            console.log("Exito cambiando build.html");
+          });
+        })
+        .catch((err) => {
+          // Manejo de error
+          console.error(err);
+        });
+
+      const html = fs.readFileSync(__dirname + "/build.html", "utf8");
+      res.end(html);
+    } else if (url == "/api/cliente") {
+      const html = fs.readFileSync(__dirname + "/build.html", "utf8");
+      res.end(html);
+    }
+
+    const html = fs.readFileSync(__dirname + "/build.html", "utf8");
+
+     fs.readFile("build.html", (err, data) => {
+       if (err) {
+         throw err;
+       }
+       console.log(data.toString());
+       res.end(data);
+     });
+  })
+  .listen(8081);
